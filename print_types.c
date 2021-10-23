e273737c8c543c9266b8955031c3ee7992f97fa9
MiDDiz <xxmidone30xx@gmail.com>
2021-10-23 12:31:21 +0200

    forget update

--- print_types.c
+++ print_types.c
@@ -6,11 +6,18 @@
 /*   By: jnaftana <jnaftana@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/23 11:55:57 by jnaftana          #+#    #+#             */
-/*   Updated: 2021/10/23 11:56:20 by jnaftana         ###   ########.fr       */
+/*   Updated: 2021/10/23 12:30:47 by jnaftana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+/*
+ * Prints in:
+ * Decimal - if num is not preceded by anything.
+ * Octal - if num is preceded by zero.
+ * 
+ */
 
 void p_type_num(int octal_num, size_t *ptr_n_printed)
 {
